25e77b01356492d3ab31245db0cd11534697cfff
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2018-09-15 14:34:19 -0300

    Final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 var ul = document.querySelector("ul");
 var input = document.querySelector("input");
+var addButton = document.querySelector("#add-button");
 
 function init(){
 
@@ -11,12 +12,28 @@ function init(){
 
 //aux functions
 function addListeners(){
-    input.addEventListener('keypress', function(event){
-        if(event.which === 13){
-            new Todo(this.value, ul); // creates a new todo
-            this.value = ""; // sets input to blank;
-        }
-    });
+    input.addEventListener('keypress', inputListener);
+
+    addButton.addEventListener('click', addButtonListener);
+}
+
+function inputListener(event){
+    if(event.which === 13 && this.value.replace(/ /g, "") !== ""){
+        new Todo(this.value, ul); // creates a new todo
+        this.value = ""; // sets input to blank;
+    }
+}
+
+function addButtonListener(){
+    if(this.classList.contains("fa-plus")){
+        this.classList.remove("fa-plus");
+        this.classList.add("fa-caret-up");
+    } else{
+        this.classList.remove("fa-caret-up");
+        this.classList.add("fa-plus");
+    }
+
+    input.classList.toggle("hide");
 }
 
 // starts

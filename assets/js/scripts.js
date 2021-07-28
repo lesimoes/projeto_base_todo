cc59f2049e2d15c7d12a15d5b7dac8aa484be427
lesimoes <lesimoes0@gmail.com>
2021-07-27 23:41:59 -0300

    finalizado: parentNode, querySelectorAll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,9 +4,7 @@ function finalizarTarefa (elemento) {
 
     const li = elemento.parentNode;
     const eFinalizada = li.classList.contains("finalizada");
-    const elementoContador = document.querySelector("h1");
-        
-
+    
     if (eFinalizada) {
         li.classList.remove("finalizada");
         contador ++;
@@ -14,8 +12,25 @@ function finalizarTarefa (elemento) {
         
     } else {
         li.classList.add("finalizada");
-        contador --;
-        elementoContador.innerHTML = `To-do List (${contador})`
+        contador --;   
     }
+
+    atualizarContador();
     
+}
+
+function atualizarContador () {
+    const elementoContador = document.querySelector("h1");
+    elementoContador.innerHTML = `To-do List (${contador})`
+}
+
+function limparTarefas () {
+    const tarefas = document.querySelectorAll("li");
+    let index = 0;
+    while (index < tarefas.length) {
+        tarefas[index].classList.remove("finalizada");
+        index ++;
+    }
+    contador = 0;
+    atualizarContador();
 }
